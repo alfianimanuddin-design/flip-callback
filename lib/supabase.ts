596eaf33ecec3a950e1d03a694f47369f7d22ae4
FLP5AlfianImanuddin <FLP5AlfianImanuddin@FLIPITID00761---ALFI.local>
2025-10-22 15:11:27 +0700

    Fix supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://dummy.supabase.co";
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://unywqywhahnnlftloqvm.supabase.co";
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "dummy-key-for-build";
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVueXdxeXdoYWhubmxmdGxvcXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NTY4NTAsImV4cCI6MjA3NjUzMjg1MH0.t0kJjQ1mBzstddyXOLbpAy8Yc_oYwmp-4u4fRjMVels";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
