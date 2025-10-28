fdbcf1288fc0b80244dddee0004e38514b0c3176
FLP5AlfianImanuddin <FLP5AlfianImanuddin@FLIPITID00761---ALFI.local>
2025-10-28 20:20:53 +0700

    Update lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,3 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://unywqywhahnnlftloqvm.supabase.co";
-const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVueXdxeXdoYWhubmxmdGxvcXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NTY4NTAsImV4cCI6MjA3NjUzMjg1MH0.t0kJjQ1mBzstddyXOLbpAy8Yc_oYwmp-4u4fRjMVels";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClientComponentClient();
